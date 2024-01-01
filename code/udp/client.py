759c64a09ca10032b84ef40f07c0780f726d155f
root <onur.yuce@metu.edu.tr>
2024-01-01 14:21:16 +0000

    udp client

--- code/udp/client.py
+++ code/udp/client.py
@@ -1,6 +1,6 @@
 import socket
 
- 
+
 
 msgFromClient       = "Hello UDP Server"
 
@@ -10,24 +10,43 @@ serverAddressPort   = ("172.17.0.2", 20001)
 
 bufferSize          = 1024
 
- 
 
-# Create a UDP socket at client side
 
-UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+def run_client():
 
- 
 
-# Send to server using created UDP socket
+    # Create a UDP socket at client side
 
-UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
- 
 
-msgFromServer = UDPClientSocket.recvfrom(bufferSize)
 
- 
+    # Send to server using created UDP socket
 
-msg = "Message from Server {}".format(msgFromServer[0])
+    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
-print(msg)
+    i = 0
+
+    while(i < 1000):
+
+        msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+
+
+
+        msg = int(msgFromServer[0].decode('utf-8'))
+
+        if(msg != i):
+            UDPClientSocket.sendto(str.encode("NACK"), serverAddressPort)
+            print(f"Missing {i}")
+            print(msg)
+        else:
+            UDPClientSocket.sendto(str.encode("ACK"), serverAddressPort)
+            print(f"Received {i}")
+            print(msg)
+            i+=1
+
+
+
+if __name__ == "__main__":
+    run_client()
+
